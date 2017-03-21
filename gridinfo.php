120092007da2069cdcda4b451a871eb1fb4f9fca
Charles Jackson <nightbeacons@gmail.com>
2017-03-21 01:02:29 +0000

    Force download of the sectional PDF

--- gridinfo.php
+++ gridinfo.php
@@ -17,7 +17,7 @@ $fpl = "fpl/" . $coordinates[$sectional]['Abbrev'] . "_FPL.zip";
 echo "	<ul>\n";
 echo "	<li><a href=\"$kmz\">Google Earth grid overlay for $sectionalName</a></li>\n";
 echo "	<li><a href=\"http://www.gelib.com/maps/_NL/aeronautical-charts-united-states.kml\">Google Earth overlays for Sectional and Terminal charts</a></li>\n";
-echo "	<li><a href=\"$mapDownload\">Download gridded $sectionalName sectional</a> <i>(PDF)</i>\n";
+echo "	<li><a href=\"$mapDownload\" download>Download gridded $sectionalName sectional</a> <i>(PDF)</i>\n";
 echo "	<li><a href=\"$cap_es_url\" target=\"_blank\">CAP-ES Grid Page for $sectionalName</a></li>\n";
 echo "	<li><a href=\"$fpl\">Download Garmin 695 Flight Plan package for $sectionalName sectional</a></li>\n";
 echo "	</ul>\n";
