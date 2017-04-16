f0b388c7884dea5d7d0d1571f690e03471df67e6
Charles Jackson <nightbeacons@gmail.com>
2017-04-16 02:37:00 +0000

    Updated help files

--- help/resources.php
+++ help/resources.php
@@ -1,4 +1,5 @@
 <div style="padding:10px;color:#303030;margin-top:30px;"><p>This section shows a satellite map layered over a terrain map.</p><p>Use the slider to adjust the transparency of the layers.</p><p>You may also download an overlay to use with Google Earth.</p>
 <p>This overlay is compatible with the <a href="http://www.gelib.com/maps/Sectionals/Aero_Charts.kml">Sectional and Terminal Chart overlays</a>.</p>
 <p>Click the <b>Print</b> button (above) to print the Google Map and grid coordinates on one page.</p>
+<img style="display:block; margin: 0 auto;border:1px solid black;" src="/images/sample.jpg" alt="sample CAPgrids.com printout"><p style="text-align:center;margin-top:4px;"><i>Sample printout</i></p>
 </div>
