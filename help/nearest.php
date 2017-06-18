5147d8ee0711a3fa3a356b3d450c5c161f088ab8
Charles Jackson <nightbeacons@gmail.com>
2017-06-18 22:28:17 +0000

    Adjusted for airnav

---
+++ help/nearest.php
@@ -0,0 +1,9 @@
+<div style="color:#303030;margin-top:30px;"><p style="padding:10px;">This section shows the five nearest airports. Private, military, and public airfields are included. Distances are measured from the center of the grid.</p><p style="padding:10px;">Airport information is provided by <a href="https://www.airnav.com/" target="_blank">Airnav.com</a></p>
+<div style="margin:0;padding:0;display:block;width:20px;">
+<a href="http://airnav.com/adclick?RR" target="itunes"><IMG style="display:block;margin:0;padding:0;" src="/images/airnav/999-1.png?v=MHVIGY" 
+width=240  alt="" border="0"></a><a href="//www.youtube.com/watch?v=qf3Mfz_RErc" 
+target="youtube"><IMG style="display:block;margin:0;padding:0;" src="/images/airnav/999-2.png?v=MHVIGZ" width=240 height=72 alt="" border="0"></a>
+</div>
+
+
+</div>
