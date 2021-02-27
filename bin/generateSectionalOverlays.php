97a8682c6368a52151697ac3a877862c1e32c54b
Charles Jackson <nightbeacons@gmail.com>
2021-02-27 19:10:35 +0000

    Added extensive debug code

--- bin/generateSectionalOverlays.php
+++ bin/generateSectionalOverlays.php
@@ -9,6 +9,8 @@
  * Detecting all-white pngs -- see http://www.imagemagick.org/discourse-server/viewtopic.php?t=30614
  */
 
+define('DEBUG', 0);
+
 // Set to 1 to refresh all chart data, regardless of Edition number.
 define('FETCH_ALL', 0);
 
@@ -58,9 +60,9 @@ function getTiff($geoname, $baseDir) {
   $geoname_No_Spaces = str_replace(" ", "_", $geoname);
   $url = "https://soa.smext.faa.gov/apra/vfr/sectional/chart?geoname=" . urlencode($geoname) . "&edition=current&format=tiff";
   $headers = ['Accept: application/json'];
-
-  echo "Fetching $geoname\n";
-
+  if (DEBUG ==1) {
+    echo "Fetching $geoname from $url\n";
+  }
   // Get JSON for path to the downlad.
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $url);
@@ -80,10 +82,17 @@ function getTiff($geoname, $baseDir) {
   }
 
   $info = json_decode($json);
+  if (DEBUG ==1){
+    $returned_json = print_r($info, TRUE);
+    echo "JSON received from $url is \n$json\an and decoded is $returned_json\n\n";
+  }
   if (isset($info->status->code)) {
     if ($info->status->code == "200") {
       $zipfilename = $baseDir . $geoname_No_Spaces . "/" . $geoname_No_Spaces . ".zip";
       $dirname = dirname($zipfilename);
+        if (DEBUG ==1){
+          echo "zipfilename = $zipfilename\n\n";
+        }
       if (!is_dir($dirname)) {
         mkdir($dirname, 0755, TRUE);
       }
@@ -104,32 +113,54 @@ function getTiff($geoname, $baseDir) {
       $faa_edition_date = faaDate2SQLdate($info->edition[0]->editionDate);
 
       // If our edition does not match the FAA edition, (or if FETCH_ALL is set) get the FAA edition.
+      if (DEBUG ==1){
+        echo "FAA edition date is $faa_edition_date\n";
+      }
       if (($faa_edition != $my_edition) or (FETCH_ALL == 1)) {
-
+            if (DEBUG ==1){
+              echo "FAA edition date does not match ours\n   FAA = $faa_edition\n   Ours = $my_edition\n\n";
+            } 
         // Remove the older .htm files
         $cmd1 = "/bin/rm " . $dirname . "/*.htm";
         $tmp = `$cmd1`;
 
         // Download the zipfile.
+        $zipfile_url = trim($info->edition[0]->product->url);
+        if (DEBUG ==1){
+          echo "Downloading the zipfile from $zipfile_url\n";
+        }
         $fh = fopen($zipfilename, 'w');
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $info->edition[0]->product->url);
+        curl_setopt($ch, CURLOPT_URL, $zipfile_url);
         curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($ch, CURLOPT_FILE, $fh);
         curl_exec($ch);
+        $curlError = trim(curl_error($ch));
         curl_close($ch);
         fclose($fh);
+         if (strlen($curlError) > 1) {
+           echo "CURL Error: $curlError fetching $zipfile_url\n\n";
+         }
 
         // Unzip the file.
         $zip = new ZipArchive();
         $zip->open($zipfilename);
+          if (DEBUG ==1){
+            echo "Unzipping $zipfilename to $dirname . . . \n";
+          }
         for ($i = 0; $i < $zip->numFiles; $i++) {
           $filename = $zip->getNameIndex($i);
           $parts = pathinfo($filename);
+          if (DEBUG ==1){
+            echo "Checking for tif extension . . . \n";
+          }
           if (($parts['extension'] == 'tif') and (strpos($parts['filename'], $geoname) !== FALSE)) {
             $tiffFilename = $dirname . "/" . $filename;
+             if (DEBUG ==1){
+             echo "TIF filename is $tiffFilename\n";
+             }
           }
         }
         $zip->extractTo($dirname);
@@ -145,13 +176,24 @@ function getTiff($geoname, $baseDir) {
           }
           // Also check -expand rgba.
           $cmd = "/usr/bin/gdal_translate -of KMLSUPEROVERLAY -expand rgba '" . $tiffFilename . "' $kml -co format=$output_format";
+             if (DEBUG ==1){
+             echo "Running gdal_translate on $tiffFilename to create KML called $kml in format $output_format\n     $cmd\n";
+             }
           $tmp = `$cmd`;
 
           // Add the current edition number to the <name> of the KML file
+             if (DEBUG ==1){
+             echo "Reading contents of $kml\n";
+             }
           $kml_contents = file_get_contents($kml);
           $searchTerm = "<name>" . $geoname_No_Spaces . "</name>";
           $replacement = "<name>" . $geoname . " Sectional " . $faa_edition . "</name>";
           $kml_contents = str_replace($searchTerm, $replacement, $kml_contents);
+          
+             if (DEBUG ==1){
+             echo "Writing revised contents of $kml\n";
+             }
+
           file_put_contents($kml, $kml_contents);
 
           // Get the date of the next edition.
@@ -159,6 +201,11 @@ function getTiff($geoname, $baseDir) {
 
           // Update MySQL tables.
           $query = "UPDATE coordinates SET editionNumber='" . $faa_edition . "', editionDate='" . $faa_edition_date . "', nextDate='" . $nextDate . "'  WHERE FullName='" . $geoname . "' LIMIT 1";
+
+             if (DEBUG ==1){
+             echo "Running query $query\n";
+             }
+
           echo "\n$query\n";
           if (!$db->query($query)) {
             echo "\nquery failed: (" . $db->errno . ") " . $db->error;
@@ -168,9 +215,15 @@ function getTiff($geoname, $baseDir) {
 
         // Remove ZIP, TIFF and TFW files.
         $cmd1 = "/bin/rm " . $dirname . "/*.tif && /bin/rm " . $dirname . "/*.tfw && /bin/rm " . $dirname . "/*.zip";
+             if (DEBUG ==1){
+                echo "Removing ZIP, TIFF and TFW files\n $cmd1\n\n";
+             }
         $tmp = `$cmd1`;
 
         // Optimize/Compress JPG files.
+             if (DEBUG ==1){
+               echo "Compressing JPGs\n";
+             }
         $findCmd = "find $dirname -print | grep -i \".jpg$\"";
         $fileAry = array_filter(explode(PHP_EOL, `$findCmd`));
         foreach ($fileAry as $filename) {
@@ -178,6 +231,9 @@ function getTiff($geoname, $baseDir) {
           $group = filegroup($filename);
           $lastMod = filemtime($filename);
           $cmd1 = "/usr/bin/jpegtran -copy none -progressive -optimize -perfect -outfile \"$filename\" \"$filename\"";
+             if (DEBUG ==1){
+               echo "     $cmd1\n";
+             }
           $tmp1 = `$cmd1`;
           touch($filename, $lastMod);
           chown($filename, $owner);
@@ -224,6 +280,9 @@ function getTiff($geoname, $baseDir) {
   // Return relative path to the generated KML file
   //  such as Baltimore.kml.
   $kml_relative = $geoname_No_Spaces . ".kml";
+       if (DEBUG ==1){
+          echo "KML Relative Path is $kml_relative\n";
+       }
   return($kml_relative);
 }
 
@@ -262,6 +321,9 @@ function getNextEditionDate($geoname) {
       $faa_next_date = faaDate2SQLdate($info->edition[0]->editionDate);
     }
   }
+       if (DEBUG ==1){
+          echo "Next FAA edition for $geoname is $faa_next_date\n";
+       }
   return($faa_next_date);
 }
 
@@ -283,8 +345,13 @@ function faaDate2SQLdate($faa_date) {
  * just updated.
  */
 function writeLatestSectionalIncludeFile($db, $latestSectionalFile){
-
+       if (DEBUG ==1){
+       echo "Writing to Include file at $latestSectionalFile\n";
+       }
 $fh = fopen($latestSectionalFile, "w");
+      if ($fh == FALSE){
+        echo "Unable to open $latestSectionalFile for writing.\n";
+      }
 $count=0;
 $query = "SELECT FullName, Abbrev, editionNumber, DATE_FORMAT(editionDate, '%e-%M-%Y') AS editionDate from coordinates
    WHERE editionDate = (select distinct editionDate from coordinates  order by editionDate desc limit 1)
