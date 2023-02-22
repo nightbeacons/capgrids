5dffae53a2a829979b66c0442cb68ad82c91dbe3
Charles Jackson <nightbeacons@gmail.com>
2023-02-22 19:56:45 +0000

    Fix typo

--- htdocs/gridinfo.php
+++ htdocs/gridinfo.php
@@ -31,7 +31,7 @@ $fpl = "fpl/" . $coordinates[$sectional]['Abbrev'] . "_FPL.zip";
 echo "	<ul>\n";
 echo "	<li><a href=\"$kmz\">Google Earth grid overlay for $sectionalName</a> <i>(with latest sectional)</i></li>\n";
 //echo "	<li><a href=\"http://www.gelib.com/maps/_NL/aeronautical-charts-united-states.kml\">Google Earth overlays for Sectional and Terminal charts</a></li>\n";
-echo "	<li><a href=\"$mapDownload\" download>Download gridded $sectionalName sectional</a> <i>(PDF)</i>\n";
+echo "	<li><a href=\"$mapDownload\" target=\"_blank\" download>Download gridded $sectionalName sectional</a> <i>(PDF)</i>\n";
 echo "	<li><a href=\"$cap_es_url\" target=\"_blank\">CAP-ES Grid Page for $sectionalName</a></li>\n";
 echo "	<li><a href=\"$fpl\">Download Garmin 695 Flight Plan package for $sectionalName sectional</a></li>\n";
 echo "	</ul>\n";
