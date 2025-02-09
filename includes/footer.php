524295bdc10604f5911ea0307177c9fb4a41a98d
Charles Jackson <nightbeacons@gmail.com>
2025-02-09 20:49:33 +0000

    cleanup

--- includes/footer.php
+++
@@ -1,2 +0,0 @@
-<hr>
-<div class="coord" style="color:#909090; font-size:12px;margin-top:20px;margin-bottom:20px;"><i>CAPgrids</i> developed by <a class="credit" style="color:#909090;" href="mailto:nightbeacons@gmail.com">Capt Charles Jackson</a></div>
--- includes/top.php
+++
@@ -1,5 +0,0 @@
-<!-- <table border="0" cellpadding="0" cellspacing="0" width="850"><tr><td><a href="/"><img width="849" height="199" border="0" src="/images/banner.png"></a>
-<center><hr></center>
-</td></tr>
-</table>  --> 
-<div class="header_image" style="width:849px;margin:0;"><a href="/"><img style="width:849;height:201; border-style:none; margin:0;" height="201" border="0" src="/images/banner.jpg"></a><hr></div>
