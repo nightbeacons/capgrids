6f7989706c5328c61beb7e9d67367d395ff6ed78
Charles Jackson <nightbeacons@gmail.com>
2025-02-09 20:46:49 +0000

    Cleanuu

--- help/findcorners.php
+++
@@ -1,4 +0,0 @@
-<div style="padding:10px;color:#303030"><p>Select a sectional and a grid identifier.</p>
-<p>The grid's average magnetic variation is calculated from the current <a href="http://www.ngdc.noaa.gov/geomag-web/calculators/" target="_blank">NOAA model</a> and adjusted for yearly drift.</p><p>It may not agree with the isogonic lines printed on aeronautical charts.</p>
-</div>
-
--- help/findgrid.php
+++
@@ -1,3 +0,0 @@
-<div style="padding:10px;color:#303030"><p>Use this section if you have Lon/Lat coordinates and need to find the associated grid.</p>
-<p>We'll find the grid, then provide the grid corners, below.</p>
-</div>
